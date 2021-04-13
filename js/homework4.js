26372196f97615c27eb84d1b24dfba4385a8d2a0
Mnemozin <mnemozinoff@gmail.com>
2021-04-14 00:10:24 +0500

    first task done

---
+++ js/homework4.js
@@ -0,0 +1,17 @@
+let transformNumToObj = (num) => {
+  if (num > 999 | num < 0) {
+    console.log('incorrect number');
+    return {};
+  }
+  let hundereds = Math.floor(num / 100);
+  num = num % 100;
+  let dozens = Math.floor(num / 10);
+  num = num % 10;
+  let ones = num / 1;
+  return {
+    единицы: ones,
+    десятки: dozens,
+    сотни: hundereds
+  }
+};
+console.log(transformNumToObj(937));
